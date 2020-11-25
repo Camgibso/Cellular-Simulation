e5f50ea8d65e9c66cb881271b7c742ac37cbd858
Camgibso <33638485+Camgibso@users.noreply.github.com>
2020-11-24 23:35:24 -0600

    update

--- main.py
+++ main.py
@@ -1,69 +1,64 @@
 # Cameron Gibson
 # R11424503
 
-# Provides access to any command-line arguments via the sys.argv 
 import sys, getopt
 
+def parseCommandLine(argv=[]):
+    argv = argv[1:]
+    inputFile = ''
+    outputFile = ''
+	# Default = 1
+    threadNum = '1'	
 
-def main(argv):
-	print("Project :: R11424503\n")
-	
-	# Set user args
-	inputfile = ''
-	outputfile = ''
+    try:
+          opts, args = getopt.getopt(argv, 'hi:o:t:')
+    except getopt.GetoptError:
+        print ("Requires -i <inputFile> -o <outputFile> -t <optionalThreads>")
+        sys.exit(1)
 
-	try:
-  		opts, args = getopt.getopt(argv,"hi:o:", ["ifile=", "ofile="])
-	except getopt.GetoptError:
-		print ('test.py -i <inputfile> -o <outputfile>')
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == '-h':
-				print ('main.py -i <inputfile> -o <outputfile>')
-				sys.exit()
-		elif opt in ("-i", "--ifile"):
-				inputfile = arg
-		elif opt in ("-o", "--ofile"):
-				outputfile = arg
-	
-	print ('Input file is: ', inputfile)
-	print ('Output file is: ', outputfile)
+    #Processing command line arguments
+    #print opts
+    for opt, arg in opts:
+        opt = opt.lower()
+        if opt in ("-h", "--help"):
+            print ("Format: Main.py -i <inputFile> -o <outputFile> -t <numThreads>")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputFile = arg 
+        elif opt in ("-o", "--ofile"):
+            outputFile = arg 
+        elif opt in ("-t", "--threadNum"):
+            threadNum = int(arg) 
+        
+    print ('Input file is: ', inputFile)
+    print ('Output file is: ', outputFile)
+    print ('Thread Number is: ', threadNum)
+	#return(inputFile)
 
 
-	# Open if exists, if not error
-	try:
-		f = open(inputfile)
+def main():
+	parseCommandLine(sys.argv)
+	#print("Returned: ", inputFile)
+	#try:
+	#	f = open(inputfile)
 		# Read a matrix from a file 
-		f.read()
+	#	f.read(inputfile)
     
-		f.close()
+	#	f.close()
 
 	# If file path DNE error
-	except FileNotFoundError:
-		print("File does not exist")
+	#except FileNotFoundError:
+	#	print("File does not exist")
 	
 	# Write the matrix into a file
-	try:
-		with open(outputfile, 'w') as f:
-			f.write(outputfile)
+	#try:
+	#	with open(outputfile, 'w') as f:
+	#		f.write(outputfile)
 	
 	# If file path DNE error
-	except DirNotFoundError:
-		print("Directory does not exist")
+	#except NotADirectoryError:
+	#	print("Directory does not exist")
 
-
+# For repl.it
 if __name__ == '__main__':
-    main(sys.argv[1:])
-
-
-#TODO:
-# Read only O's and .'s else error
-
-# Validate user args
-# get -> define -> retrieve
-
-# If an “alive” square has exactly two, three, or four living neighbors, then it continues to be “alive” in the next time step.
-
-# If a “dead” square has an even number greater than 0 living neighbors, then it will be “alive” in the next time step.
-
-# Every other square dies or remains dead, causing it to be “dead” in the next time step.
+	main()
