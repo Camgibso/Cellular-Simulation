a5e8a38f8ac519a2634bc6b46054726d3ebf7150
Cam <cameron.gibson@ttu.edu>
2020-11-25 01:15:34 +0000

    updated

---
+++ main.py
@@ -0,0 +1,28 @@
+# Cameron Gibson
+# R11424503
+
+# Provides access to any command-line arguments via the sys.argv 
+import sys
+
+
+def main():
+	print("Project :: R11424503\n")
+
+
+if __name__ == '__main__':
+    main()
+
+
+#TODO:
+
+# Read a matrix from a file 
+# Write the matrix into a file 
+
+# Validate user args
+# get -> define -> retrieve
+
+# If an “alive” square has exactly two, three, or four living neighbors, then it continues to be “alive” in the next time step.
+
+# If a “dead” square has an even number greater than 0 living neighbors, then it will be “alive” in the next time step.
+
+# Every other square dies or remains dead, causing it to be “dead” in the next time step.
